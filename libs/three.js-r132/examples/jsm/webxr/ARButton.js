4da8ca5d5cb957e7273181c8af3b420c99fef2a5
Nathan Barnes <nbarnes@azahner.com>
2022-05-15 14:43:33 -0500

    update

--- libs/three.js-r132/examples/jsm/webxr/ARButton.js
+++ libs/three.js-r132/examples/jsm/webxr/ARButton.js
@@ -140,7 +140,7 @@ class ARButton {
 			element.style.padding = '12px 6px';
 			element.style.border = '1px solid #fff';
 			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
+			element.style.background = 'rgba(234,24,0,1)';
 			element.style.color = '#fff';
 			element.style.font = 'normal 13px sans-serif';
 			element.style.textAlign = 'center';
