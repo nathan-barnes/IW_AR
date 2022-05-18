590f07b632c9fc0966ea56b881512dd6742f388a
Nathan Barnes <nbarnes@azahner.com>
2022-05-18 14:09:59 -0500

    IW animation

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    // const gltf = await loadGLTF('./assets/models/IW/scene.gltf');
-    const gltf = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
+    const gltf = await loadGLTF('./assets/models/IW/scene.gltf');
+    // const gltf = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
     console.log(gltf)
     gltf.scene.scale.set(0.1, 0.1, 0.1);
     gltf.scene.position.set(0, -0.4, 0);
